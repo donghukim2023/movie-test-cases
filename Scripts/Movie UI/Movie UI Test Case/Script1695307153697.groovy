9c6466f9fba9b3d3728a1898ca614f37e5e7810c
dankim <dan.donghu.kim@gmail.com>
2023-09-22 00:40:26 +0900

    commit

--- Scripts/Movie UI/Movie UI Test Case/Script1695307153697.groovy
+++ Scripts/Movie UI/Movie UI Test Case/Script1695307153697.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.openBrowser('http://130.162.141.190/')
 
 WebUI.navigateToUrl('http://130.162.141.190/')
 
